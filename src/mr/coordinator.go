56fa2824649e9c9251d7f8030e71fb38c38b39c5
m4tu4g <71326926+m4tu4g@users.noreply.github.com>
2024-10-30 13:35:46 +0530

    feat: [test] add `MapTask`

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -6,28 +6,43 @@ import "os"
 import "net/rpc"
 import "net/http"
 
+const Debug = true
+
+func DPrintf(format string, a ...interface{}) (n int, err error) {
+	if Debug {
+		log.Printf(format, a...)
+	}
+	return
+}
 
 type Coordinator struct {
 	// Your definitions here.
-
+	files []string
 }
 
 // Your code here -- RPC handlers for the worker to call.
 
-//
+func (c *Coordinator) GetMapTask(args *GetMapTaskArgs, reply *GetMapTaskReply) error {
+	var f string
+	if len(c.files) > 0 {
+		f, c.files = c.files[0], c.files[1:]
+		DPrintf("Sent file %s\n", f)
+	} else {
+		DPrintf("No files to send")
+	}
+	reply.Filename = f
+	return nil
+}
+
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
-
-//
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -39,31 +54,27 @@ func (c *Coordinator) server() {
 		log.Fatal("listen error:", e)
 	}
 	go http.Serve(l, nil)
+	DPrintf("server listening at %v", sockname)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
 	ret := false
 
 	// Your code here.
 
-
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
 
 	// Your code here.
-
+	c.files = files
 
 	c.server()
 	return &c
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -5,44 +5,35 @@ import "log"
 import "net/rpc"
 import "hash/fnv"
 
-
-//
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
 // main/mrworker.go calls this function.
-//
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
 	// Your worker implementation here.
+	GetMapTask()
 
 	// uncomment to send the Example RPC to the coordinator.
 	// CallExample()
 
 }
 
-//
 // example function to show how to make an RPC call to the coordinator.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func CallExample() {
 
 	// declare an argument structure.
@@ -67,11 +58,9 @@ func CallExample() {
 	}
 }
 
-//
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
@@ -88,4 +77,27 @@ func call(rpcname string, args interface{}, reply interface{}) bool {
 
 	fmt.Println(err)
 	return false
+}
+
+func MapWorker(mapf func(string, string) []KeyValue, reducef func(string, []string) string) {
+
+}
+
+func ReduceWorker(mapf func(string, []string) string, reducef func(string, []string) string) {
+
+}
+
+func GetMapTask() {
+	reply := GetMapTaskReply{}
+
+	ok := call("Coordinator.GetMapTask", GetMapTaskArgs{}, &reply)
+	if ok {
+		if reply.Filename != "" {
+			fmt.Printf("Got file %v\n", reply.Filename)
+		} else {
+			fmt.Printf("No files atm\n")
+		}
+	} else {
+		DPrintf("call failed!\n")
+	}
 }
